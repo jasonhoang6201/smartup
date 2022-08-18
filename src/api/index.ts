bd8f53f70cb77f1e1a8a2c3d70c0aa4039c8aa63
Jason Hoang <jasonhoang6201@gmail.com>
2022-08-18 21:50:25 +0700

    change ai

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,37 +1,37 @@
-import axios from "axios"
-import queryString from "query-string"
-import { deleteCookie } from "src/helpers/cookie"
+import axios from "axios";
+import queryString from "query-string";
+import { deleteCookie } from "src/helpers/cookie";
 
-const token = localStorage.getItem("token")
+const token = localStorage.getItem("token");
 
 const apiConfig = {
-    baseURL: 'http://localhost:3001/',
-    token: token ? `${token}` : '',
-}
+  baseURL: "https://ec01-03-server.herokuapp.com/",
+  token: token ? `${token}` : "",
+};
 
 const axiosClient = axios.create({
-    baseURL: apiConfig.baseURL,
-    headers: {
-        "Content-Type": "application/json",
-        token: apiConfig.token,
-    },
-    paramsSerializer: params => queryString.stringify({ ...params })
-})
+  baseURL: apiConfig.baseURL,
+  headers: {
+    "Content-Type": "application/json",
+    token: apiConfig.token,
+  },
+  paramsSerializer: (params) => queryString.stringify({ ...params }),
+});
 
-axiosClient.interceptors.request.use(async (config) => config)
-axiosClient.interceptors.response.use((response) => {
+axiosClient.interceptors.request.use(async (config) => config);
+axiosClient.interceptors.response.use(
+  (response) => {
     if (response && response.data) {
-        return response.data
+      return response.data;
     }
-    return response
-}, (error) => {
+    return response;
+  },
+  (error) => {
     if (error?.response?.status === 401) {
-        localStorage.removeItem("token");
-        deleteCookie("token");
+      localStorage.removeItem("token");
+      deleteCookie("token");
     }
-})
+  }
+);
 
-
-export {
-    axiosClient,
-}
+export { axiosClient };
